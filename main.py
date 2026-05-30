04e67168fd53fa43c5cbf45c82992cbf391ef857
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 09:10:43 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class smsUeleteWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = smsUeleteWorker()
+    print("Worker engine initialized.")
